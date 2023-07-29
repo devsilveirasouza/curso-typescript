5505d23344258fcc44a954639c7abc60556679cf
devsilveirasouza <dev.silveirasouza@gmail.com>
2023-07-29 08:03:25 -0300

    Configurações iniciais

---
+++ README.md
@@ -0,0 +1,9 @@
+# CURSO DE TYPESCRIPT
+
+## Instalação do Typescript global
+
+npm install -g typescript
+
+## Instalar apenas no projeto
+
+npm install typescript
--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let numero1 = document.getElementById('numero1');
-let numero2 = document.getElementById('numero2');
-let botao = document.getElementById('numero1');
-let res = document.getElementById('resultado');
-
+var numero1 = document.getElementById('numero1');
+var numero2 = document.getElementById('numero2');
+var botao = document.getElementById('numero1');
+var res = document.getElementById('resultado');
 function calcular(n1, n2) {
-    if (typeof n1 == 'number' && typeof n1 == 'number') {
-        return n1 + n2;
-    } else {
-        n1 = parseInt(n1);
-        n2 = parseInt(n2);
-        return n1 + n2;
-    }
+    return n1 + n2;
 }
-
-botao.addEventListener('click', function() {
-    res.innerHTML = calcular( numero1.value, numero2.value );
+botao.addEventListener('click', function () {
+    res.innerHTML = calcular(numero1.value, numero2.value);
 });
