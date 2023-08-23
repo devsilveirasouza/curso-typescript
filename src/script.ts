c74b745bc74d8aecd27e1f245c8d6f1ca4054709
devsilveirasouza <dev.silveirasouza@gmail.com>
2023-08-23 15:50:21 -0300

    Types no retorno de função

--- src/script.ts
+++ src/script.ts
@@ -9,8 +9,25 @@
 
 // Type any = qualquer tipo //
 // let nomes: string[] = ['Pedro', 'Maria', 'Cristina', 'Luana'];
-let nomes: any = ["Pedro", "Maria", "Cristina", "Luana"];
-nomes.push(556);
+// let nomes: any = ["Pedro", "Maria", "Cristina", "Luana"];
+// nomes.push(556);
 
-let coisas: any = ['Paulo', 59, 'Mary', true];
-coisas.push(759);
+// let coisas: any = ['Paulo', 59, 'Mary', true];
+// coisas.push(759);
+
+// Usando types no retorno de uma função // 
+// function firstLetterUpperCase(name: string): string {
+//     let firstLetter = name.charAt(0).toUpperCase();
+//     return firstLetter+name.substring(1);
+// }
+// let nome: string = firstLetterUpperCase('wellington');
+
+// function somar(n1: number, n2: number):number {
+//     return n1 + n2;
+// }
+// let alguma = somar(90, 35);
+
+// function somar1(n1: number, n2: number) {
+//     return `${n1 + n2}`;
+// }
+// let outra = somar(45, 72);
