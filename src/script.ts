790062260c0b8b33704b87756f7349e804af1883
devsilveirasouza <dev.silveirasouza@gmail.com>
2023-08-23 15:34:14 -0300

    Type any

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,16 @@
 // DECLARANDO ARRAYS
-let nomes: string[] = ['Well', 'Ton', 'Brenda', 'Jana'];
+// let nomes: string[] = ['Well', 'Ton', 'Brenda', 'Jana'];
 
-let idades: number[] = [25, 35, 24, 15];
+// let idades: number[] = [25, 35, 24, 15];
 
-let numeros: Array<number> = [15, 23, 90, 11];
-// INSERIR DADOS NO ARRAY
-nomes.push('Souza');
+// let numeros: Array<number> = [15, 23, 90, 11];
+// // INSERIR DADOS NO ARRAY
+// nomes.push('Souza');
+
+// Type any = qualquer tipo //
+// let nomes: string[] = ['Pedro', 'Maria', 'Cristina', 'Luana'];
+let nomes: any = ["Pedro", "Maria", "Cristina", "Luana"];
+nomes.push(556);
+
+let coisas: any = ['Paulo', 59, 'Mary', true];
+coisas.push(759);
